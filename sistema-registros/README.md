de3e708a05346bc72498dfb7c0ce9cdbd744a23e
Wanessa <wanessa.ant04@gmail.com>
2023-09-27 17:32:13 -0300

    1 commit

---
+++ sistema-registros/README.md
@@ -0,0 +1,34 @@
+Santander Bootcamp 2023 - Backend Java - Desafios 
+
+Desafio de Código = Sistema Informativo
+
+Codifique o seguinte cenário.
+
+
+Neste desafio, você criará uma solução para um sistema bancário utilizando conceitos de orientação a objetos. 
+
+A implementação solicitada pela equipe de desenvolvimento da empresa bancária, gostaria que fosse realizado uma melhoria no sistema de registros, onde é recebido os dados brutos dos registros bancários. O objetivo é obter as informações de uma forma mais amigável para o cliente e não oferecer informações brutas.
+
+Considere que cada transação é representada por apenas uma string contendo as seguintes informações: data,hora,descrição,valor.
+
+Dessa forma, crie uma classe representando as Transações e nela será recebido os atributos necessários para guardar as informações, ao fim, retorne o registro contendo a data, hora e valor da transação.
+
+Entrada:
+- A entrada será a representação de um registro (string) no formato e sequencia: "data,hora,descricao,valor".
+
+Saída:
+- A saída deve ser o valor do registro (string) recebido na entrada informando a transação efetuada, contendo, descrição, data, hora e valor da transação.
+
+Exemplos:
+1. Entrada:
+
+01/02/2023,09:00:00,Deposito,100,00
+
+1. Saída:
+Deposito
+
+01/02/2023
+
+09:00:00
+
+100,00
---
+++ sistema-registros/src/SistemaInformativo.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class SistemaInformativo {
+    
+  public static void main(String[] args) {
+    try (Scanner scanner = new Scanner(System.in)) {
+      String entrada = scanner.nextLine();
+      String[] partes = entrada.split(",");
+
+      // Obtenção dos valores da entrada.
+      String data = partes[0];
+      String hora = partes[1];
+      String descricao = partes[2];
+      double valor = Double.parseDouble(partes[3]);
+
+      // Criando uma instância de Transação com os valores da entrada.
+      Transacao transacao = new Transacao(data, hora, descricao, valor);
+      
+      // Chamando o método imprimir para exibir as informações formatadas.
+      transacao.imprimir();
+    } catch (NumberFormatException e) {
+     
+      e.printStackTrace();
+    }
+  }
+}
+
+class Transacao {
+  private String data;
+  private String hora;
+  private String descricao;
+  private double valor;
+
+  public Transacao(String data, String hora, String descricao, double valor) {
+    this.data = data;
+    this.hora = hora;
+    this.descricao = descricao;
+    this.valor = valor;
+  }
+  
+  public void imprimir() {
+    System.out.println(this.descricao);
+    System.out.println(this.data);
+    System.out.println(this.hora);
+    System.out.printf("%.2f", this.valor);
+  }
+}
